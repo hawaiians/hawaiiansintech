5274ce48aaaa895c30c37fee3080eac3008476ed
Taylor Ho <taylorkmho@gmail.com>
2024-03-24 17:53:42 -0700

    updated styling of directory

--- pages/admin/directory.tsx
+++ pages/admin/directory.tsx
@@ -178,7 +178,7 @@ const Directory: MemberDirectoryType = ({ members, regions, user }) => {
     DirectorySortOrder.LastModified,
   );
   const [error, setError] = useState<ErrorMessageProps>(null);
-  const [filteredMembers, setFilteredMembers] = useState<MemberPublic[]>();
+  const [filteredMembers, setFilteredMembers] = useState<MemberPublic[]>([]);
 
   useEffect(() => {
     setFilteredMembers(
@@ -210,77 +210,86 @@ const Directory: MemberDirectoryType = ({ members, regions, user }) => {
 
   return (
     <>
-      <div className="sticky top-0 z-50 w-full bg-tan-400">
-        <div className="mx-auto flex w-full max-w-5xl flex-wrap items-center gap-1 px-2 py-1">
-          <div className="flex grow items-center gap-2">
-            <h2 className="text-xl font-semibold leading-8">Directory</h2>
-            <Tabs
-              defaultValue={Object.values(DirectoryFilter)[0]}
-              onValueChange={(value) => {
-                setTabVisible(value as DirectoryFilter);
-              }}
-              value={tabVisible}
+      <div className="mx-auto max-w-4xl gap-2 flex flex-col py-8 px-4">
+        <h2 className="text-2xl font-semibold">Directory</h2>
+        <div className="flex gap-2 items-center flex-wrap">
+          <Tabs
+            defaultValue={Object.values(DirectoryFilter)[0]}
+            onValueChange={(value) => {
+              setTabVisible(value as DirectoryFilter);
+            }}
+            value={tabVisible}
+          >
+            <TabsList loop>
+              {Object.values(DirectoryFilter).map((filter, i) => (
+                <TabsTrigger value={filter} key={`directory-filter-${i}`}>
+                  {filter}
+                </TabsTrigger>
+              ))}
+            </TabsList>
+          </Tabs>
+          <div>
+            <Select
+              onValueChange={(val: DirectorySortOrder) => setSortOrder(val)}
+              defaultValue={sortOrder}
             >
-              <TabsList loop>
-                {Object.values(DirectoryFilter).map((filter, i) => (
-                  <TabsTrigger value={filter} key={`directory-filter-${i}`}>
-                    {filter}
-                  </TabsTrigger>
-                ))}
-              </TabsList>
-            </Tabs>
-            <div className="flex grow justify-end">
-              <select
-                className="rounded px-1 py-0.5 text-sm"
-                value={sortOrder}
-                onChange={(e) =>
-                  setSortOrder(e.target.value as DirectorySortOrder)
-                }
-              >
-                {Object.values(DirectorySortOrder).map((option) => (
-                  <option key={`sort-order-${option}`} value={option}>
-                    {option}
-                  </option>
-                ))}
-              </select>
-            </div>
+              <SelectTrigger className="h-8 gap-x-1">
+                <SelectValue placeholder={sortOrder ?? "Sorting"} />
+              </SelectTrigger>
+              <SelectContent>
+                {Object.values(DirectorySortOrder).map(
+                  (option: DirectorySortOrder) => (
+                    <SelectItem value={option} key={`sort-order-${option}`}>
+                      {option}
+                    </SelectItem>
+                  ),
+                )}
+              </SelectContent>
+            </Select>
           </div>
+          {filteredMembers?.length > 0 && (
+            <h4 className="text-base font-medium text-secondary-foreground shrink-0 grow text-right">
+              {filteredMembers.length} kanaka
+            </h4>
+          )}
         </div>
-      </div>
-      <div className="mx-auto flex w-full flex-col">
-        {error && (
-          <div className="mx-auto my-2 w-full max-w-5xl">
-            <ErrorMessage
-              headline={error.headline}
-              body={error.body}
-              onClose={() => {
-                setError(null);
-              }}
-            />
-          </div>
-        )}
-        {filteredMembers && filteredMembers.length > 0 ? (
-          <>
-            {filteredMembers.map((m) => (
-              <Directory.Card
-                member={m}
-                key={`member-card-${m.id}`}
-                regions={regions}
-                user={user}
+        <div className="flex flex-col gap-2">
+          {error && (
+            <div className="mx-auto my-2 w-full max-w-5xl">
+              <ErrorMessage
+                headline={error.headline}
+                body={error.body}
+                onClose={() => {
+                  setError(null);
+                }}
               />
-            ))}
-          </>
-        ) : (
-          <div className="flex w-full justify-center p-4">
-            <LoadingSpinner variant={LoadingSpinnerVariant.Invert} />
-          </div>
-        )}
+            </div>
+          )}
+          {filteredMembers && filteredMembers.length > 0 ? (
+            <>
+              {filteredMembers.map((m) => (
+                <Directory.Card
+                  member={m}
+                  key={`member-card-${m.id}`}
+                  regions={regions}
+                  user={user}
+                  className="rounded-2xl border-2"
+                />
+              ))}
+            </>
+          ) : (
+            <div className="flex w-full justify-center p-4">
+              <LoadingSpinner variant={LoadingSpinnerVariant.Invert} />
+            </div>
+          )}
+        </div>{" "}
       </div>
     </>
   );
 };
 
 interface CardProps {
+  className?: string;
   member: MemberPublic;
   regions?: DocumentData[];
   user?: User;
@@ -288,7 +297,7 @@ interface CardProps {
 
 Directory.Card = Card;
 
-function Card({ member, regions, user }: CardProps) {
+function Card({ className, member, regions, user }: CardProps) {
   const [showModal, setShowModal] = useState<ReactNode | false>(false);
 
   const handleDelete = async () => {
@@ -316,19 +325,20 @@ function Card({ member, regions, user }: CardProps) {
       <Dialog>
         <DialogTrigger
           className={cn(
-            "group border-b",
+            "group shadow-md",
             member.status === StatusEnum.APPROVED
-              ? "border-tan-300 hover:bg-tan-600/5 active:bg-tan-600/10"
+              ? "border-tan-300/50 hover:border-tan-300 hover:bg-tan-600/5 active:bg-tan-600/10"
               : member.status === StatusEnum.IN_PROGRESS
-              ? "border-violet-500/30 bg-violet-500/5 hover:bg-violet-500/10 active:bg-violet-500/20"
+              ? "border-violet-500/30 shadow-violet-500/20 bg-violet-500/5 hover:bg-violet-500/10 active:bg-violet-500/20"
               : member.status === StatusEnum.PENDING
               ? "border-amber-500/30 bg-amber-500/5 hover:bg-amber-500/10 active:bg-amber-500/20"
               : "border-red-500/30 bg-red-500/5 hover:bg-red-500/10 active:bg-red-500/20",
+            className,
           )}
         >
           <div
             className={cn(
-              "mx-auto flex w-full max-w-5xl items-center gap-2 px-2 py-4",
+              "mx-auto flex w-full max-w-5xl items-center gap-2 p-4",
             )}
           >
             <div
@@ -370,19 +380,10 @@ function Card({ member, regions, user }: CardProps) {
                   {member.id}
                 </h3> */}
                 </div>
-                {/* <span
-                className="text-xs font-light text-secondary-foreground"
-                title={`${member.lastModified}`}
-              >
-                Last modified{" "}
-                <span className={"font-semibold text-stone-700"}>
-                  {moment(member.lastModified).fromNow()}
-                </span>
-              </span> */}
               </div>
               <div
                 className={cn(
-                  "grid grid-flow-col grid-cols-5 items-start gap-2 rounded bg-tan-500/10 px-4 py-2 text-xs",
+                  "grid grid-flow-col grid-cols-4 items-start gap-2 rounded bg-tan-500/10 px-4 py-2 text-xs",
                   member.status === StatusEnum.IN_PROGRESS &&
                     "bg-violet-500/10",
                 )}
@@ -397,12 +398,6 @@ function Card({ member, regions, user }: CardProps) {
                   <h4 className="font-medium">Region</h4>
                   <p className="break-words font-light text-secondary-foreground">
                     {member.region}
-                  </p>
-                </section>
-                <section>
-                  <h4 className="font-medium">Company Size</h4>
-                  <p className="break-words font-light text-secondary-foreground">
-                    {member.companySize}
                   </p>
                 </section>
                 <section>
@@ -455,6 +450,12 @@ function Card({ member, regions, user }: CardProps) {
                   </p>
                 </section>
               </div>
+              <span
+                className="text-xs font-light text-secondary-foreground"
+                title={`${member.lastModified}`}
+              >
+                Last modified {moment(member.lastModified).fromNow()}
+              </span>
             </div>
           </div>
         </DialogTrigger>
