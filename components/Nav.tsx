760bd35decb6f72e507151d9a5acf3abe002acb8
Taylor Ho <taylorkmho@gmail.com>
2024-04-04 14:05:36 -0700

    first pass

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,6 +1,19 @@
 import { Icon, IconAsset, IconColor } from "@/components/icon/icon";
 import Logo from "@/components/Logo";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+export enum NavAppearance {
+  NavShown = "show",
+  NavMinimized = "min",
+}
+
+const navLogoVariants = {
+  floatLeft: { x: -40 },
+  default: { x: 0 },
+};
 
 interface NavProps {
   backUrl?: string;
@@ -11,20 +24,33 @@ interface NavProps {
 }
 
 export default function Nav({ backUrl, children, primaryNav }: NavProps) {
+  const router = useRouter();
+  const { prev } = router.query;
+
   let logo = <Logo />;
   if (backUrl) {
     logo = (
-      <Link
-        href={"/"}
-        className="transition-transform hover:scale-105 active:scale-95"
+      <motion.a
+        whileHover={{ scale: 1.05 }}
+        whileTap={{ scale: 0.95 }}
+        animate={navLogoVariants.default}
+        initial={
+          prev === NavAppearance.NavShown
+            ? navLogoVariants.floatLeft
+            : navLogoVariants.default
+        }
+        href="/"
+        variants={navLogoVariants}
       >
         {logo}
-      </Link>
+      </motion.a>
     );
   }
   return (
-    <header className="flex w-full items-center justify-between gap-4 p-4 sm:pl-8">
-      <nav className="flex items-center gap-8">
+    <header className="flex w-full items-center justify-between gap-8 p-4 sm:pl-8">
+      <nav
+        className={cn("flex items-center gap-4", primaryNav?.show && "gap-8")}
+      >
         {backUrl ? (
           <Link href={backUrl} shallow={true}>
             <div className="transition-transform hover:scale-105 active:scale-95">
@@ -37,19 +63,25 @@ export default function Nav({ backUrl, children, primaryNav }: NavProps) {
           <>
             <Link
               className="text-base font-medium text-stone-700"
-              href="/about"
+              href={`/about?prev=${NavAppearance.NavShown}`}
             >
               About
             </Link>
-            <Link href="/hackathon" className="font-script text-2xl">
+            <Link
+              href={`/hackathon?prev=${NavAppearance.NavShown}`}
+              className="font-script text-2xl"
+            >
               Hackathon
             </Link>
           </>
         ) : null}
       </nav>
-      {children ? <div className="grow">{children}</div> : null}
-      <Link
-        className={`
+      {children ? (
+        <div className="flex items-center grow gap-4">{children}</div>
+      ) : null}
+      {primaryNav?.show && (
+        <Link
+          className={`
           rounded-lg
           border-4
           border-tan-300
@@ -68,10 +100,11 @@ export default function Nav({ backUrl, children, primaryNav }: NavProps) {
           sm:px-4
           sm:py-2
         `}
-        href="/join/00-aloha"
-      >
-        Join us
-      </Link>
+          href={`/join/00-aloha?prev=${NavAppearance.NavShown}`}
+        >
+          Join us
+        </Link>
+      )}
     </header>
   );
 }
