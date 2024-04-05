522cdce2b78fb0cfba6f45050a4220f84cbcbe35
Taylor Ho <taylorkmho@gmail.com>
2024-04-05 10:40:23 -0700

    added ONBOARDING_OPTIONS object
    - using this var formatting for magic values

--- pages/join/00-aloha.tsx
+++ pages/join/00-aloha.tsx
@@ -7,6 +7,14 @@ import { Button } from "@/components/ui/button";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import Head from "next/head";
 import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+
+enum OnboardingSelection {
+  HawaiianInTech = "hwn",
+  Ally = "ally",
+  None = "none",
+}
 
 export async function getStaticProps() {
   return {
@@ -16,7 +24,56 @@ export async function getStaticProps() {
   };
 }
 
+const ONBOARDING_OPTIONS: {
+  type: OnboardingSelection;
+  emoji: string;
+  title: string;
+  bullets: string[];
+}[] = [
+  {
+    type: OnboardingSelection.HawaiianInTech,
+    emoji: "✊🏽",
+    title: "Join as a Native Hawaiian in Tech",
+    bullets: [
+      "Connect with kanaka who share your interests",
+      "Grow your career through mentorship and advice",
+      "Inspire the next generation to pursue tech",
+    ],
+  },
+  {
+    type: OnboardingSelection.Ally,
+    emoji: "🫂",
+    title: "Support as an ally",
+    bullets: [
+      "Support your kanaka friends and family",
+      "Stay informed on our events and progress",
+      "Share your skills to empower our community",
+    ],
+  },
+];
+
 export default function Aloha({ pageTitle }) {
+  const router = useRouter();
+  const [selected, setSelected] = useState<OnboardingSelection>(
+    OnboardingSelection.None,
+  );
+
+  const handleSubmit = () => {
+    switch (selected) {
+      case OnboardingSelection.Ally:
+        console.log("yeah");
+        break;
+      case OnboardingSelection.HawaiianInTech:
+        router.push("01-you");
+        break;
+
+      case OnboardingSelection.None:
+      default:
+        console.log("yeeeeeeeeeeeeeeeah");
+        break;
+    }
+  };
+
   return (
     <>
       <Head>
@@ -30,39 +87,27 @@ export default function Aloha({ pageTitle }) {
       </Heading>
 
       <div className="mx-auto max-w-xl px-8 space-y-4">
-        <RadioGroup defaultValue="option-one" className="space-y-2">
-          {[
-            {
-              emoji: "✊🏽",
-              title: "Join as a Native Hawaiian in Tech",
-              bullets: [
-                "Connect with kanaka who share your interests",
-                "Grow your career through mentorship and advice",
-                "Inspire the next generation to pursue tech",
-              ],
-            },
-            {
-              emoji: "🫂",
-              title: "Support us as an ally",
-              bullets: [
-                "Support your kanaka friends and family",
-                "Stay informed on our events and progress",
-                "Share your skills to empower our community",
-              ],
-            },
-          ].map((item, index) => {
-            const { emoji, title, bullets } = item;
+        <RadioGroup
+          defaultValue={selected}
+          onValueChange={(type: OnboardingSelection) => setSelected(type)}
+          className="space-y-2"
+        >
+          {ONBOARDING_OPTIONS.map((item, index) => {
+            const { type, emoji, title, bullets } = item;
 
             return (
               <>
-                <div className="border-2 border-primary/30 rounded-xl shadow-lg">
+                <div
+                  className="border-2 border-primary/20 border-t-primary/10 rounded-xl shadow-lg"
+                  key={`option-container-${index}-${emoji}`}
+                >
                   <Label
-                    htmlFor={`choice-${index}`}
+                    htmlFor={type}
                     className="flex items-center gap-x-4 px-4 py-6 pb-6"
                   >
                     <RadioGroupItem
-                      value={`choice-${index}`}
-                      id={`choice-${index}`}
+                      value={type}
+                      id={type}
                       variant="outline"
                       size="lg"
                     />
@@ -73,7 +118,9 @@ export default function Aloha({ pageTitle }) {
                       <h3 className="text-xl font-semibold">{title}</h3>
                       <ul className="space-y-2">
                         {bullets.map((bullet, index) => (
-                          <li>{bullet}</li>
+                          <li className="" key={`bullet-${bullet}-${index}`}>
+                            {bullet}
+                          </li>
                         ))}
                       </ul>
                     </div>
@@ -90,7 +137,13 @@ export default function Aloha({ pageTitle }) {
               Login
             </Link>
           </p>
-          <Button size="lg">Continue</Button>
+          <Button
+            onClick={handleSubmit}
+            disabled={selected === OnboardingSelection.None}
+            size="lg"
+          >
+            Continue
+          </Button>
         </div>
       </div>
     </>
