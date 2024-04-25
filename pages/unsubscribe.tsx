0bc63ec448535514975b086b208da83138340bc9
Taylor Ho <taylorkmho@gmail.com>
2024-04-24 22:48:30 -0700

    added state to unsub flow

--- pages/unsubscribe.tsx
+++ pages/unsubscribe.tsx
@@ -4,15 +4,19 @@ import { useIsAdmin } from "@/lib/hooks";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/lib/firebase";
-import { Button } from "@/components/ui/button";
+import { Button, buttonVariants } from "@/components/ui/button";
 import LoadingSpinner, {
   LoadingSpinnerVariant,
 } from "@/components/LoadingSpinner";
+import Logo from "@/components/Logo";
+import Link from "next/link";
+import { DISCORD_URL } from "./about";
+import Code from "@/components/Code";
 
 export default function UnsubscribePage() {
-  const [unsubStatus, setUnsubStatus] = useState<"loading" | "success">(
-    "loading",
-  );
+  const [unsubStatus, setUnsubStatus] = useState<
+    "loading" | "success" | "error"
+  >("loading");
   const [error, setError] = useState<string>(null);
   const [user] = useAuthState(auth);
   const router = useRouter();
@@ -66,6 +70,7 @@ export default function UnsubscribePage() {
       setUnsubStatus("success");
     } catch (error) {
       console.error("Failed to unsubscribe:", error.message);
+      setUnsubStatus("error");
       setError(error.message);
     }
   };
@@ -77,16 +82,68 @@ export default function UnsubscribePage() {
   }, [query]);
 
   return (
-    <>
-      {!error && unsubStatus === "loading" && (
-        <div className="flex w-full justify-center p-4">
-          <LoadingSpinner variant={LoadingSpinnerVariant.Invert} />
+    <section
+      className={`
+        mx-auto
+        mb-4
+        mt-8
+        flex
+        max-w-lg
+        flex-col
+        px-4
+      `}
+    >
+      {/* <Button onClick={handleGenerate}>Generate</Button> */}
+      <div className="flex w-full flex-col gap-4 sm:rounded-lg sm:border sm:p-4">
+        <div className="self-center">
+          <Logo />
         </div>
-      )}
-      {unsubStatus === "success" && <p>Successfully unsubscribed</p>}
-      {error && <p>Did not unsubscribe {error}</p>}
-      <Button onClick={handleGenerate}>Generate</Button>
-      <Button onClick={handleUnsubscribe}>Unsubscribe</Button>
-    </>
+        {unsubStatus === "loading" && (
+          <div className="self-center py-12">
+            <LoadingSpinner variant={LoadingSpinnerVariant.Invert} />
+          </div>
+        )}
+        <h2 className="grow text-2xl">
+          {unsubStatus === "success" ? (
+            <>Hūlō!</>
+          ) : unsubStatus === "error" ? (
+            <>E kala mai</>
+          ) : null}
+        </h2>
+
+        <div className="flex flex-col gap-2 text-base text-secondary-foreground">
+          {error && (
+            <p>
+              Something went wrong here. The dang machine keeps buzzing{" "}
+              <Code>{error}</Code>.
+            </p>
+          )}
+          {unsubStatus === "success" && (
+            <>
+              <p>
+                You’ve unsubscribed from our mailing list. You won’t receive any
+                more newsletter updates from us.
+              </p>
+              <p>You can subscribe again at any time.</p>
+            </>
+          )}
+        </div>
+        <div className="flex flex-col gap-2">
+          {error ? (
+            <Link className={buttonVariants()} href={DISCORD_URL}>
+              Let us know on Discord
+            </Link>
+          ) : null}
+          {unsubStatus !== "loading" && (
+            <Link
+              className={buttonVariants({ variant: "secondary" })}
+              href={"/"}
+            >
+              Back to home
+            </Link>
+          )}
+        </div>
+      </div>
+    </section>
   );
 }
